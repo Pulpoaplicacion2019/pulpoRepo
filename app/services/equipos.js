c8fb570f3aa0b93fa1bc367fd4d0fec01c419c7c
Nathali <nathali.mejia@clearminds-it.com>
2019-11-21 23:00:15 -0500

    se agrega pantalla

--- app/services/equipos.js
+++ app/services/equipos.js
@@ -1,12 +1,19 @@
 import firebase from 'react-native-firebase';
 
-export const loadTeams =(object,lista,index)=>{
+export const loadTeams =(object,categoria)=>{
+	console.log("ingresa a cargar equipos");
+   var itemsRef = firebase.database().ref('equipos');
+   var rEq =itemsRef.child(global.idTorneo+'/categorias/'+categoria+'/equipos');
+	listenForItemsEquipos(rEq,object);
+
+}	
+export const saveTeams =(object,lista)=>{
 	console.log("ingresa a cargar equipos");
    var itemsRef = firebase.database().ref('equipos');
    var rEq =itemsRef.child(global.idTorneo+'/categorias/'+lista[index]+'/equipos');
 	listenForItemsEquipos(rEq,object);
 
-}	
+}
  listenForItemsEquipos = (itemsRef,object) => {
     itemsRef.on('value', (snap) => {
 
